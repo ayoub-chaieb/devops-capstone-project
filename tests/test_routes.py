9f10676572cc81a760c4fa86fc2e7853be449113
Ayoub <ayoubchaieb75@gmail.com>
2026-01-16 17:31:08 -0500

    Added Test for Read Account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,4 +123,27 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should Read an Account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.post(
+            f"{BASE_URL}/{account.id}",
+            json=account.serialize(),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.get(f"/accounts/{account.id}")
+        # Check the data is correct
+        new_account = response.get_json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(new_account["name"], account.name)
+        self.assertEqual(new_account["email"], account.email)
+        self.assertEqual(new_account["address"], account.address)
+        self.assertEqual(new_account["phone_number"], account.phone_number)
+        self.assertEqual(new_account["date_joined"], str(account.date_joined))
+
+        response = self.client.get(f"/accounts/{99}")
+        new_account = response.get_json()
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        
